30dfc3901a7d4d30c64badd0ab73efc6d0f302b0
olson@student.ubc.ca <olson@student.ubc.ca>
2023-05-02 13:28:27 -0700

    Initial commit

---
+++ Extractor/Program.cs
@@ -0,0 +1,21 @@
+﻿using Extractor;
+using TreeBasedCli;
+
+var settings = new ArgumentHandlerSettings
+(
+    name: "IRSS Video Tools",
+    version: "0.0.1",
+    commandTree: new CommandTree(
+        root: CreateCommandTreeRoot())
+);
+
+
+var argumentHandler = new ArgumentHandler(settings);
+await argumentHandler.HandleAsync(args);
+
+
+Command CreateCommandTreeRoot()
+{
+    Command extractCommand = new ExtractCommand();
+    return extractCommand;
+}
