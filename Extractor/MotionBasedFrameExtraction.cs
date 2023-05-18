9a8f2c9ce4b4055fc3a2f7753858a1ae5d0de588
Lukas Olson <lukasdragon@live.ca>
2023-05-18 12:21:27 -0700

    added using statements
    Signed-off-by: Lukas Olson <lukasdragon@live.ca>

--- Extractor/MotionBasedFrameExtraction.cs
+++ Extractor/MotionBasedFrameExtraction.cs
@@ -11,21 +11,21 @@ public static class MotionBasedFrameExtraction
     // Calculates the amount of frame overlap between two frames using EmGU CV
     public static double CalculateFrameMovement(ImageData previousFrame, ImageData currentFrame)
     {
-        var frame1 = previousFrame.ToEmguBitmap().Convert<Gray, byte>();
-        var frame2 = currentFrame.ToEmguBitmap().Convert<Gray, byte>();
+        using var frame1 = previousFrame.ToEmguBitmap().Convert<Gray, byte>();
+        using var frame2 = currentFrame.ToEmguBitmap().Convert<Gray, byte>();
 
-        var flow = new Mat();
+        using var flow = new Mat();
 
-        CvInvoke.CalcOpticalFlowFarneback(frame1, frame2, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+        CvInvoke.CalcOpticalFlowFarneback(frame1, frame2, flow, 0.5, 3, 15, 3, 5, 1.1, 0);
 
 
-        var flowX = new Mat(frame1.Size, DepthType.Cv32F, 1);
-        var flowY = new Mat(frame1.Size, DepthType.Cv32F, 1);
+        using var flowX = new Mat(frame1.Size, DepthType.Cv32F, 1);
+        using var flowY = new Mat(frame1.Size, DepthType.Cv32F, 1);
         CvInvoke.Split(flow, new VectorOfMat(flowX, flowY));
 
 
-        var magnitude = new Mat();
-        var angle = new Mat();
+        using var magnitude = new Mat();
+        using var angle = new Mat();
         CvInvoke.CartToPolar(flowX, flowY, magnitude, angle);
 
         var totalMagnitude = CvInvoke.Sum(magnitude).V0;
@@ -40,7 +40,7 @@ public static class MotionBasedFrameExtraction
     }
 
 
-    // Adapted from https://stackoverflow.com/a/20198278
+    // Theory adapted from https://stackoverflow.com/a/20198278
     public static double CalculateImageSharpness(ImageData frame)
     {
         using var greyscaleImage = frame.ToEmguBitmap().Convert<Gray, byte>();
@@ -48,7 +48,6 @@ public static class MotionBasedFrameExtraction
         using var cannyFilteredImage = greyscaleImage.Canny(255, 175);
 
         var imageEdgeCount = cannyFilteredImage.CountNonzero()[0];
-
 
         double imagePixelCount = cannyFilteredImage.Cols * cannyFilteredImage.Rows;
         var sharpness = imageEdgeCount / imagePixelCount * 100;
